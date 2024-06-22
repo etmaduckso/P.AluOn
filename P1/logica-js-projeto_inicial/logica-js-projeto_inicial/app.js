79b7b85e844b3133b924faaab68faf90d8110141
Sr. Lake <84886995+etmaduckso@users.noreply.github.com>
2024-06-22 16:54:52 -0300

    Incluindo op. Ternário

--- P1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ P1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,13 +1,18 @@
 alert('Bem vido ao jogo N° Secreto!');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
 //WHILE --> É USADO PARA REPÉTIR A OPERAÇÃO ENQUANTO O RESULTADO OU A CONDIÇÃO DA FUNÇÃO NÃO FOR ATINGIDO.
+
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
+
     // Condição para validar a escolha do número secreto no prompt.
+
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+       break;
     }
     else {
         if (chute > numeroSecreto) {
@@ -16,6 +21,18 @@ while (chute != numeroSecreto) {
         else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        //No ALERT do IF foi inserido um contador de tentativas onde será evidenciado quantas tentativas foram realizadas até o acerto do N°.S --> Esse é o cod. para inserir o contador (tentativas = tentativas +1) a baixo a forma simplificada.
+        tentativas++;
     }
     //Template Strings - usando o sinal de crase (`) e o sinal de cifrão ($) seguido de ({}) ele vai retornar o valor da variavel declarada como base para a função.
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+//O CODIGO ACIMA É A VERSÃO SIMPLIFICADA DO CODIGO ABAIXO USANDO OPERADOR TERNÁRIO.
+
+// if (tentativas > 1) {
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// }
+// else  alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
