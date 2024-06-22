373ec0ad7a67386c5f49f79f314ad25d769f75ab
Sr. Lake <84886995+etmaduckso@users.noreply.github.com>
2024-06-22 15:48:24 -0300

    incluido operadores e func.

---
+++ P1/logica-js-projeto_inicial/app.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+alert ('Bem-vindo ao nosso site!');
+let nome = "Lua";
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = ('Erro! Preencha todos os campos');
+alert(mensagemDeErro);
+let nomeUsuario = prompt('Digite o seu nome');
+let idadeUsuario = prompt('Digite a sua idade');
+if (idadeUsuario < 18) {
+    alert('Não pode tirar a habilitação!');
+}
+if (idadeUsuario >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}
--- P1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ P1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,1 +1,21 @@
-alert('Alura | ONE');
+alert('Bem vido ao jogo N° Secreto!');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute
+//WHILE --> É USADO PARA REPÉTIR A OPERAÇÃO ENQUANTO O RESULTADO OU A CONDIÇÃO DA FUNÇÃO NÃO FOR ATINGIDO.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // Condição para validar a escolha do número secreto no prompt.
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+    }
+    else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
+    //Template Strings - usando o sinal de crase (`) e o sinal de cifrão ($) seguido de ({}) ele vai retornar o valor da variavel declarada como base para a função.
+}
