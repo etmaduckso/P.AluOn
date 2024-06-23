0974ec1ceab5352c30fc2310fbd899b6e6cb7ac7
Sr. Lake <84886995+etmaduckso@users.noreply.github.com>
2024-06-22 21:57:46 -0300

    Alterações gerais na estrutura / inclusão de GNSecreto - 2 (manipulando HTML usando JS

---
+++ P1/g_n_secreto2/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
