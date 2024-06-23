576c251e99acdd0889f78bf5391628fc97ed1197
Sr. Lake <84886995+etmaduckso@users.noreply.github.com>
2024-06-23 15:03:14 -0300

    conf. VRF.chutes/ inserido contador de tentativas/ BT. Reiniciar hab/ aut. BT. reiciar.

--- P1/g_n_secreto2/app.js
+++ P1/g_n_secreto2/app.js
@@ -3,18 +3,28 @@
 
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//--------------------------------------------------------------------
 
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+
+//--------------------------------------------------------------------
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+//--------------------------------------------------------------------
 
-exibirTextoNaTela('h1', 'Jogo do número secreto')
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+//A declaração a baixo realiza a função que é mostrada acima.
 
+exibirMensagemInicial();
+
+//---------------------------------------------------------------------
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -24,6 +34,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas.' : 'tentativa.';
         let mensagemTentativa = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativa);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }
     else {
         if (chute > numeroSecreto) {
@@ -34,12 +45,32 @@ function verificarChute() {
         }
         //tentativas = tentativas +1;
         tentativas++;
-
+        limparCampo();
     }
 }
+
+//----------------------------------------------------------------------
+
 
 // O comando (parseInt) quando usado com (Math.random) gera N° aleatorios inteiros sem esse comando  o (Math.random) gera numeros decimais de 0 a 1. 
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+//------------------------------------------------------------------------
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+//-------------------------------------------------------------------------
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
 }
