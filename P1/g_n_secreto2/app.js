b6a86778dbcb154ff9d9002b89d829e0fd79053b
Sr. Lake <84886995+etmaduckso@users.noreply.github.com>
2024-06-23 22:49:34 -0300

    habilitado responsiveVoice link no HTML. linha 7

--- P1/g_n_secreto2/app.js
+++ P1/g_n_secreto2/app.js
@@ -3,18 +3,22 @@
 
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-//--------------------------------------------------------------------
+//-----------------------------------------------------------------
 
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-//--------------------------------------------------------------------
+//-----------------------------------------------------------------
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
-//--------------------------------------------------------------------
+
+//-----------------------------------------------------------------
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
@@ -24,7 +28,7 @@ function exibirMensagemInicial() {
 
 exibirMensagemInicial();
 
-//---------------------------------------------------------------------
+//-----------------------------------------------------------------
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -49,23 +53,37 @@ function verificarChute() {
     }
 }
 
-//----------------------------------------------------------------------
+//-----------------------------------------------------------------
 
 
 // O comando (parseInt) quando usado com (Math.random) gera N° aleatorios inteiros sem esse comando  o (Math.random) gera numeros decimais de 0 a 1. 
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNalista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNalista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio;
+    }
+    else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+    
 }
 
-//------------------------------------------------------------------------
+//-----------------------------------------------------------------
 
 function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
 
-//-------------------------------------------------------------------------
+//-----------------------------------------------------------------
 
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
